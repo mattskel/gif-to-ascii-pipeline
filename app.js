d48308fd73c7528173f83b16d32f1292ee24994e
Matt Skelley <mattskelley.au@gmail.com>
2023-08-30 21:07:18 +0800

    Refactor server to handle errors
    Replace response buffer with socket.
    Use pipeline for parsing gif file. Allows single error handler.
    Remove write head otherwise can't set status later.
    Update fetch callback for new response format.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import {get} from 'https';
-import express from 'express';
+// import express from 'express';
 import { 
   HeaderTransform, 
   FrameHeaderTransform, 
@@ -11,6 +11,7 @@ import {
   ColorTransform,
   CanvasTransform,
 } from './myTransforms.js';
+import { pipeline } from 'stream';
 
 const fixedLength = 4;
 function myTransform(url, _res, io) {
@@ -27,38 +28,63 @@ function myTransform(url, _res, io) {
     myObject.transparentColors = [];
     myObject.comments = [];
     myObject.localColorTables = [];
-    const frameStream = res  
-      .pipe(new HeaderTransform(myObject))
-      .pipe(new FrameHeaderTransform(myObject))
-      .pipe(new FrameImageTransform(myObject))
-      .pipe(new ColorTransform(myObject))
+    // const frameStream = res  
+    //   .pipe(new HeaderTransform(myObject))
+    //   .pipe(new FrameHeaderTransform(myObject))
+    //   .pipe(new FrameImageTransform(myObject))
+    //   .pipe(new ColorTransform(myObject))
+    //   .on('error', (err) => {
+    //     _res.status(500).send({ error: 'something blew up' })
+    //     _res.end();
 
-      frameStream
-        .pipe(new CanvasTransform(myObject))
-        .on('data', (data) => {
-          io.emit('colorFrame', data.toString());
-        });
+    //     return;
+    //   });
+    // _res.status(200);
+    const frameStream = pipeline(
+      res,  
+      new HeaderTransform(myObject),
+      new FrameHeaderTransform(myObject),
+      new FrameImageTransform(myObject),
+      new ColorTransform(myObject),
+      (err) => {
+        if (!err) {
+          return;
+        }
 
-      frameStream
-        .pipe(new GreyScaleTransform(myObject))
-        .pipe(new CompressionTransform(myObject))
-        .pipe(new AsciiTransform(myObject))
-        .pipe(new PulseTransform(myObject, frames))
-        .on('data', (data) => {
-          const buffer = Buffer.allocUnsafe(fixedLength);
-          buffer.writeInt32LE(data.length, 0);
-          _res.write(Buffer.concat([buffer, data]))
-        })
-        .on('finish', () => {
-          // need to extract the delays from the frames
-          const delays = frames
-            .map(({delay}) => delay);
+        console.log('err', err);
+        _res.status(500).json({ error: 'something blew up' })
+        _res.end();
+      }
+    );
 
-          const buffer = Buffer.allocUnsafe(fixedLength);
-          buffer.writeInt32LE(delays.length, 0);
-          _res.write(Buffer.concat([buffer, new Uint8Array(delays)]));
-          _res.end();
-        })
+    frameStream
+      .pipe(new CanvasTransform(myObject))
+      .on('data', (data) => {
+        io.emit('colorFrame', data.toString());
+      });
+
+    frameStream
+      .pipe(new GreyScaleTransform(myObject))
+      .pipe(new CompressionTransform(myObject))
+      .pipe(new AsciiTransform(myObject))
+      .pipe(new PulseTransform(myObject, frames))
+      .on('data', (data) => {
+        // const buffer = Buffer.allocUnsafe(fixedLength);
+        // buffer.writeInt32LE(data.length, 0);
+        // _res.write(Buffer.concat([buffer, data]))
+        io.emit('asciiFrame', data.toString());
+      })
+      .on('finish', () => {
+        // need to extract the delays from the frames
+        const delays = frames
+          .map(({delay}) => delay);
+
+        // const buffer = Buffer.allocUnsafe(fixedLength);
+        // buffer.writeInt32LE(delays.length, 0);
+        // _res.write(Buffer.concat([buffer, new Uint8Array(delays)]));
+        _res.status(200).send(delays)
+        _res.end();
+      })
   })
     .on('error', (err) => {
       if (err) {
